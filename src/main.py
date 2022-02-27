2b86fddf3c5a87d805e5ccdc8cd75da18dad34e9
c17hawke <sunny.c17hawke@gmail.com>
2022-02-27 16:58:00 +0530

    MLproject workflow added

---
+++ src/main.py
@@ -0,0 +1,32 @@
+import mlflow
+import argparse
+import os
+import logging
+from src.utils.common import read_yaml, create_directories
+
+
+STAGE = "MAIN" ## <<< change stage name 
+
+logging.basicConfig(
+    filename=os.path.join("logs", 'running_logs.log'), 
+    level=logging.INFO, 
+    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
+    filemode="a"
+    )
+
+
+def main():
+    with mlflow.start_run() as run:
+        mlflow.run(".", "get_data", use_conda=False)
+        # mlflow.run(".", "get_data", parameters={}, use_conda=False)
+        mlflow.run(".", "base_model_creation", use_conda=False)
+
+if __name__ == '__main__':
+    try:
+        logging.info("\n********************")
+        logging.info(f">>>>> stage {STAGE} started <<<<<")
+        main()
+        logging.info(f">>>>> stage {STAGE} completed!<<<<<\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
